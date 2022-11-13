aed83c1de2a4603dd1c0cbe79a75961835c600d0
ospinacar <107015747+ospinacar@users.noreply.github.com>
2022-11-12 23:45:32 -0500

    Add files via upload

---
+++ geometry.py
@@ -0,0 +1,11 @@
+def rectangle_area(b,h):
+    """
+    This function returns the area of a rectangle of base b and height h
+    """
+    return b * h 
+
+def circle_area(radius):
+    """
+    This function returns the area of a rectangle of base b and height h
+    """
+    return 3.141592653589 * (radius ** 2)
